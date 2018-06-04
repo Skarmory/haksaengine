cdcda9d2efae81e8f7c722bf0c31199d9462f2d8
Alastair Paterson <alas.paterson@gmail.com>
2018-06-04 17:35:56 +0100

    Updated mesh.h
    Mesh now wraps OpenGL objects. It will initialise vao and vbos and bind/unbind them

--- haksaengine/haksaengine/include/gfx/mesh.h
+++ haksaengine/haksaengine/include/gfx/mesh.h
@@ -2,10 +2,19 @@
 
 #include <vector>
 
+#include "GL/glew.h"
 #include "vertex.h"
 
 struct Mesh
 {
+	std::string name;
 	std::vector<Vertex> vertices;
 	std::vector<unsigned int> indices;
+
+	void initialise(void);
+	void bind(void);
+	void unbind(void);
+
+private:
+	GLuint vertex_array, vertex_buffer, index_buffer;
 };
---
+++ haksaengine/haksaengine/src/gfx/mesh.cpp
@@ -0,0 +1,39 @@
+#include "gfx/mesh.h"
+
+// Generate vertex array object, vertex buffer, and index buffer
+void Mesh::initialise(void)
+{
+	glGenVertexArrays(1, &vertex_array);
+	glGenBuffers(1, &vertex_buffer);
+	glGenBuffers(1, &index_buffer);
+
+	glBindVertexArray(vertex_array);
+		
+		glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, position));
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, normal));
+		glEnableVertexAttribArray(1);
+		//glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)(6 * sizeof(GLfloat)));
+		//glEnableVertexAttribArray(2);
+
+	glBindVertexArray(0);
+}
+
+// Wrapper to bind vertex array
+void Mesh::bind(void)
+{
+	glBindVertexArray(vertex_array);
+}
+
+
+// Wrapper to unbind vertex array
+void Mesh::unbind(void)
+{
+	glBindVertexArray(0);
+}
