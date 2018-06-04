2343597e359b3ce48a5f0963d131de56177369d8
Alastair Paterson <alas.paterson@gmail.com>
2018-06-04 17:35:18 +0100

    Changed OpenGL version

--- haksaengine/haksaengine/src/window.cpp
+++ haksaengine/haksaengine/src/window.cpp
@@ -2,8 +2,8 @@
 
 GameWindow::GameWindow(int width, int height, const std::string& window_title)
 {
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
