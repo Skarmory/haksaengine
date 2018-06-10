14a2125e3ae71891474e31e3e2da68ea8647a2d2
Alastair Paterson <alas.paterson@gmail.com>
2018-06-10 17:13:59 +0100

    Better unit testing of BlueprintLoader

--- haksaengine/haksaengine_test/loader_tests.cpp
+++ haksaengine/haksaengine_test/loader_tests.cpp
@@ -4,6 +4,8 @@
 #include <Windows.h>
 #include <iostream>
 #include <string>
+#include <exception>
+#include <functional>
 
 #include "io/blueprint_loader.h"
 
@@ -20,28 +22,67 @@ namespace haksaengine_test
 		void load(NamedVariantPack* data) override
 		{
 			this->data = data->get("data").as_float;
+
+			Assert::AreEqual(7.77f, this->data);
 		}
 
 		float data;
 	};
 
+	struct TestComponentB : public Component<TestComponentB>
+	{
+		void load(NamedVariantPack* data) override
+		{
+			this->data1 = data->get("data1").as_int;
+			this->data2 = data->get("data2").as_float;
+
+			Assert::AreEqual(7, this->data1);
+			Assert::AreEqual(5.55f, this->data2);
+		}
+
+		int data1;
+		float data2;
+	};
+
+	struct TestComponentC : public Component<TestComponentC>
+	{
+		void load(NamedVariantPack* data) override {}
+	};
+
 	TEST_CLASS(BlueprintLoader_Test)
 	{
 	public:
+		std::string directory;
+		BlueprintLoader* bpl;
+
+		TEST_METHOD_INITIALIZE(setup)
+		{
+			directory = EXPAND(UNITTESTPRJ);
+			directory.erase(0, 1);
+			directory.erase(directory.size() - 2);
+
+			bpl = new  BlueprintLoader(directory + "..\\..\\assets\\blueprints\\");
+		}
+
+		TEST_METHOD_CLEANUP(teardown)
+		{
+			delete bpl;
+		}
+
 		TEST_METHOD(BlueprintLoader_load)
 		{
-			std::string current_dir = EXPAND(UNITTESTPRJ);
-			current_dir.erase(0, 1);
-			current_dir.erase(current_dir.size() - 2);
+			bpl->register_component<TestComponentA>("TestComponentA");
+			bpl->register_component<TestComponentB>("TestComponentB");
 
-			BlueprintLoader bpl(current_dir + "..\\..\\assets\\blueprints\\");
-
-			bpl.register_component<TestComponentA>("TestComponentA");
-
-			Blueprint* bp = bpl.load("blueprint_unittest");
+			Blueprint* bp = bpl->load("blueprint_unittest");
 
 			delete bp;
 		}
-		
+
+		TEST_METHOD(BlueprintLoader_load_unregistered_component_exception_test)
+		{
+			std::function<Blueprint*(void)> func = [this] { return bpl->load("blueprint_unittest"); };
+			Assert::ExpectException<std::runtime_error>(func);
+		}	
 	};
 }
