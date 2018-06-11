5ff77cd5570ec62e87b722f7af55c691384bd685
Alastair Paterson <alas.paterson@gmail.com>
2018-06-11 01:29:22 +0100

    Added some loading logic to AssetManager
    The AssetManager can now have Loaders added to it. It also stores Asset* by their hashed file name.
    
    It can load an asset of a given type if the Loader for that type has been created and added to it.

--- haksaengine/haksaengine/asset_manager.cpp
+++ haksaengine/haksaengine/asset_manager.cpp
@@ -1,6 +1,7 @@
 #include "asset_manager.h"
 
-#include <fstream>
+#include "io/mesh_loader.h"
+#include "gfx/mesh.h"
 
 AssetManager::AssetManager(void) : AssetManager(DEFAULT_ASSET_DIRECTORY)
 {
@@ -8,22 +9,19 @@ AssetManager::AssetManager(void) : AssetManager(DEFAULT_ASSET_DIRECTORY)
 
 AssetManager::AssetManager(const std::string& asset_directory) : _asset_directory(asset_directory)
 {
-	_mesh_loader = new MeshLoader(DEFAULT_ASSET_DIRECTORY + "models\\");
+	add_loader<Mesh>(new MeshLoader("models\\"));
 }
 
 AssetManager::~AssetManager(void)
 {
-	delete _mesh_loader;
+	for (auto pair : _loaders)
+		delete pair.second;
 }
 
 void AssetManager::set_asset_directory_path(const char* path)
 {
 	_asset_directory = path;
-}
 
-void AssetManager::load_asset(const char* asset_path)
-{
-	std::string complete_path = _asset_directory + asset_path;
-
-
+	for (auto pair : _loaders)
+		pair.second->_asset_path = _asset_directory;
 }
--- haksaengine/haksaengine/asset_manager.h
+++ haksaengine/haksaengine/asset_manager.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
+#include <typeindex>
+#include <exception>
 
-#include "io/mesh_loader.h"
+#include "io/loader.h"
 
 class AssetManager;
+class Loader;
 
 class Asset
 {
@@ -21,13 +25,51 @@ public:
 	AssetManager(const std::string& asset_directory);
 	~AssetManager(void);
 
-	void HAKSAENGINE_API set_asset_directory_path(const char* path);
-	void HAKSAENGINE_API load_asset(const char* asset_path);
-	
+	HAKSAENGINE_API void set_asset_directory_path(const char* path);
+	/*HAKSAENGINE_API const char*  get_asset_directory_path(void) const;*/
+
+	template<class C>
+	unsigned int load_asset(const char* asset_name)
+	{
+		unsigned int  id = std::hash<std::string>{}(std::string(asset_name));
+
+		// Check if we've loaded this asset already
+		if (_assets.find(id) != _assets.end())
+			return id;
+
+		std::type_index tid = typeid(C);
+
+		// Not loaded, check if we have a loader for it
+		if (_loaders.find(tid) != _loaders.end())
+		{
+			// A loader exists so load this asset
+			Asset* loaded = _loaders[tid]->load(asset_name);
+			loaded->id = id;
+			loaded->name = asset_name;
+
+			_assets[id] = loaded;
+		}
+		else
+		{
+			// No loader, throw an exception because the game probably won't work
+			//throw std::runtime_error("Error trying to load file: " + std::string(asset_name) + ". Loader for type " + std::string(tid.name) + " not found. Make sure a loader for this has been added to the AssetManager.");
+			throw std::runtime_error("Failed to load asset");
+		}
+
+		return id;
+	}
+
+	template<class C>
+	void add_loader(Loader* loader)
+	{
+		loader->_asset_path = _asset_directory;
+		_loaders[typeid(C)] = loader;
+	}
+
 private:
-
-	const std::string DEFAULT_ASSET_DIRECTORY = "..\\..\\assets\\";
-
+	const std::string DEFAULT_ASSET_DIRECTORY = "assets\\";
 	std::string _asset_directory;
-	MeshLoader* _mesh_loader;
+
+	std::unordered_map<std::type_index, Loader*> _loaders;
+	std::unordered_map<unsigned int, Asset*> _assets;
 };
