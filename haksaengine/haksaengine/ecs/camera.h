61e562e20e6be96752a0ec0a0be7a1763551ab05
Alastair Paterson <alas.paterson@gmail.com>
2018-06-15 00:55:31 +0100

    Added Camera component

---
+++ haksaengine/haksaengine/ecs/camera.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "ecs/component.h"
+
+class Camera : public Component<Camera>
+{
+	void load(NamedVariantPack* data) override
+	{
+		near_plane = data->get("near_plane").as_float;
+		far_plane = data->get("far_plane").as_float;
+		fov = data->get("fov").as_float;
+	}
+
+	float near_plane, far_plane;
+	float fov;
+};
