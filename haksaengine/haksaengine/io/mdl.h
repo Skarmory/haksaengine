8ecc35d5c8595594607d4cdffc3c1ab7a420c784
Alastair Paterson <alas.paterson@gmail.com>
2018-07-02 02:28:48 +0100

    Added bones and animations vectors to MDLFile

--- haksaengine/haksaengine/io/mdl.h
+++ haksaengine/haksaengine/io/mdl.h
@@ -5,13 +5,17 @@
 #include "asset_manager.h"
 #include "gfx/mesh.h"
 #include "gfx/texture.h"
+#include "anim/bone.h"
+#include "anim/animation.h"
 
+// Pairs mdl file data together
 struct MDLData
 {
 	unsigned int mesh_id;
 	unsigned int texture_id;
 };
 
+// Contains all the data from a .MDL file
 class MDLFile : public Asset
 {
 public:
@@ -22,6 +26,8 @@ private:
 	std::vector<Mesh*> _meshes;
 	std::vector<Texture*> _textures;
 	std::vector<MDLData> _data;
+	std::vector<Bone> _bones;
+	std::vector<Animation> _animations;
 
 	friend class MDLLoader;
 	friend class Renderer;
