f233b5715df32ba76d962dd76f1630aa32180fbb
Alastair Paterson <alas.paterson@gmail.com>
2018-07-09 20:18:25 +0100

    Added Death animation to archer.mdl
    Removed obsolete transform data

--- haksaengine/game/src/main.cpp
+++ haksaengine/game/src/main.cpp
@@ -12,6 +12,7 @@
 #include "ecs/entity.h"
 #include "ecs/component.h"
 #include "ecs/transform.h"
+#include "ecs/animator.h"
 
 class TestComponent : public Component<TestComponent>
 {
@@ -44,7 +45,17 @@ int main(int argc, char** argv)
 
 	Entity* entity = Services::get().get_entity_manager()->get_entity(entity_id);
 	Transform* transform = entity->get_component<Transform>();
+	Animator* anim = entity->get_component<Animator>();
+	anim->current_animation = "Attack";
 	transform->translate(glm::vec3(50.0f, 0.0f, 0.0f));
+
+	entity_id = Services::get().get_entity_manager()->create_entity(&test_blueprint);
+
+	entity = Services::get().get_entity_manager()->get_entity(entity_id);
+	transform = entity->get_component<Transform>();
+	anim = entity->get_component<Animator>();
+	anim->current_animation = "Death";
+	transform->translate(glm::vec3(100.0f, 0.0f, 0.0f));
 
 	e.run();
 
