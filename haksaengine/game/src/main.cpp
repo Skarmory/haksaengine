51e98929b4fb4bf0e09ac544062f61784e0418ff
Alastair Paterson <alas.paterson@gmail.com>
2018-07-10 23:06:11 +0100

    Added Idle animation for archer

--- haksaengine/game/src/main.cpp
+++ haksaengine/game/src/main.cpp
@@ -43,6 +43,9 @@ int main(int argc, char** argv)
 
 	entity = Services::get().get_entity_manager()->get_entity(entity_id);
 
+	transform = entity->get_component<Transform>();
+	transform->translate(glm::vec3(-50.0f, 0.0f, 0.0f));
+
 	player = new Player;
 	player->colour = PlayerColour::RED;
 	entity->add_component(player);
@@ -53,7 +56,7 @@ int main(int argc, char** argv)
 	entity = Services::get().get_entity_manager()->get_entity(entity_id);
 
 	transform = entity->get_component<Transform>();
-	transform->translate(glm::vec3(50.0f, 0.0f, 0.0f));
+	transform->translate(glm::vec3(0.0f, 0.0f, 0.0f));
 
 	anim = entity->get_component<Animator>();
 	anim->current_animation = "Attack";
@@ -68,14 +71,29 @@ int main(int argc, char** argv)
 	entity = Services::get().get_entity_manager()->get_entity(entity_id);
 
 	transform = entity->get_component<Transform>();
-	transform->translate(glm::vec3(100.0f, 0.0f, 0.0f));
+	transform->translate(glm::vec3(50.0f, 0.0f, 0.0f));
 
 	anim = entity->get_component<Animator>();
 	anim->current_animation = "Death";
 
 	player = new Player;
 	player->colour = PlayerColour::GREEN;
-	entity->add_component(player);	
+	entity->add_component(player);
+
+	// Created fourth archer
+	entity_id = Services::get().get_entity_manager()->create_entity(&test_blueprint);
+
+	entity = Services::get().get_entity_manager()->get_entity(entity_id);
+
+	transform = entity->get_component<Transform>();
+	transform->translate(glm::vec3(100.0f, 0.0f, 0.0f));
+
+	anim = entity->get_component<Animator>();
+	anim->current_animation = "Idle";
+
+	player = new Player;
+	player->colour = PlayerColour::RED;
+	entity->add_component(player);
 
 	e.run();
 
