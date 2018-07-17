60bbf9dd3bcc5bf77ce7a84e21f69ca58b9473b2
Alastair Paterson <alas.paterson@gmail.com>
2018-07-17 16:07:39 +0100

    Added SystemOrdering struct

---
+++ haksaengine/haksaengine/ecs/system_ordering.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// Where abouts in the update pipeline this system should be updated
+enum UpdatePriority
+{
+	PRERENDER  = 0,
+	RENDER     = 1,
+	POSTRENDER = 2
+};
+
+// Represents the ordering of system deriving classes' updating.
+// Systems with a lower index will be updated before systems with a higher index
+struct SystemOrdering
+{
+	UpdatePriority priority;
+	unsigned int index;
+};
