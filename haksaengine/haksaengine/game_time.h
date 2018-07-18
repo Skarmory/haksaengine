a265e4c686bd21919f5a37f9c5685a15fd816ec4
Alastair Paterson <alas.paterson@gmail.com>
2018-07-18 16:22:17 +0100

    Made GameTime derive from Service
    Exported GameTime functions

--- haksaengine/haksaengine/game_time.h
+++ haksaengine/haksaengine/game_time.h
@@ -3,17 +3,19 @@
 #include <chrono>
 
 #include "globals.h"
+#include "service.h"
 
 /* Class to track the elapsed time since the program started, and also
    to track delta time between frames */
-class GameTime
+class GameTime : public Service
 {
 public:
 	GameTime(void);
 
+	HAKSAENGINE_API float delta(void) const;
+	HAKSAENGINE_API float elapsed(void) const;
+
 	void tick(void);
-	float delta(void) const;
-	float elapsed(void) const;
 
 private:
 	std::chrono::time_point<std::chrono::high_resolution_clock> current;
