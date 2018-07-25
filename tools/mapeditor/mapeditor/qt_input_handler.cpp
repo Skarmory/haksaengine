04af92dd60c19eb213e22d7f91adae40601f93e2
Alastair Paterson <alas.paterson@gmail.com>
2018-07-25 22:43:24 +0100

    Added QtInputHandler class for mapeditor

---
+++ tools/mapeditor/mapeditor/qt_input_handler.cpp
@@ -0,0 +1,57 @@
+#include "qt_input_handler.h"
+
+void QtInputHandler::update(void)
+{
+
+}
+
+void QtInputHandler::process_key(void)
+{
+	/*std::string keystr = input->_key_to_string(key);
+
+	if (keystr == "NoKey")
+		return;
+
+	if (action == GLFW_PRESS)
+		input->set_key_state(keystr.c_str(), KeyState::Pressed);
+
+	else if (action == GLFW_RELEASE)
+		input->set_key_state(keystr.c_str(), KeyState::Released);*/
+}
+
+void QtInputHandler::process_mouse_move(QMouseEvent* event)
+{
+	set_mouse_position(event->x(), event->y());
+}
+
+void QtInputHandler::process_mouse_pressed(QMouseEvent* event)
+{
+	MouseButtonType type = _qt_mouse_button_convert(event->button());
+
+	set_mouse_button_state(type, MouseButtonState::Pressed);
+}
+
+void QtInputHandler::process_mouse_released(QMouseEvent* event)
+{
+	MouseButtonType type = _qt_mouse_button_convert(event->button());
+
+	set_mouse_button_state(type, MouseButtonState::Released);
+}
+
+void QtInputHandler::process_mouse_scroll(QWheelEvent* event)
+{
+	auto delta = event->pixelDelta();
+	auto adelta = event->angleDelta();
+
+	set_mouse_scroll(delta.y());
+}
+
+MouseButtonType QtInputHandler::_qt_mouse_button_convert(Qt::MouseButton button)
+{
+	switch (button)
+	{
+		case Qt::MouseButton::LeftButton: return MouseButtonType::Button_1;
+		case Qt::MouseButton::RightButton: return MouseButtonType::Button_2;
+		default: return MouseButtonType::None;
+	}
+}
---
+++ tools/mapeditor/mapeditor/qt_input_handler.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "input/input_manager.h"
+#include "input/mouse.h"
+
+#include <QMouseEvent>
+#include <QWheelEvent>
+
+class QtInputHandler : public InputManager
+{
+public:
+
+	void update(void) override;
+
+	void process_key(void);
+	void process_mouse_move(QMouseEvent* event);
+	void process_mouse_pressed(QMouseEvent* event);
+	void process_mouse_released(QMouseEvent* event);
+	void process_mouse_scroll(QWheelEvent* event);
+
+private:
+
+	MouseButtonType _qt_mouse_button_convert(Qt::MouseButton button);
+
+};
