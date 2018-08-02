3666f6b713565e1c7b83284111c598bfff8d24cd
Alastair Paterson <alas.paterson@gmail.com>
2018-08-03 00:28:59 +0100

    Added draw method to terrain
    Also added indexing and coordinate helper functions

--- haksaengine/haksaengine/scene/terrain.cpp
+++ haksaengine/haksaengine/scene/terrain.cpp
@@ -1,5 +1,7 @@
 #include "scene/terrain.h"
 
+#include "services.h"
+
 void Terrain::update(unsigned int x, unsigned int y, const TerrainVertex& vertex)
 {
 	if (x > _width || y > _height)
@@ -30,7 +32,40 @@ const TerrainVertex& Terrain::get_vertex(const glm::vec3 position) const
 	return get_vertex(tmp.x, tmp.y);
 }
 
+void Terrain::draw(void)
+{
+	_render_cmds.push_back(new BindTerrainMeshCommand(_mesh));
+	_render_cmds.push_back(new UseShaderCommand(*_shader));
+	_render_cmds.push_back(new DrawIndexedCommand(PrimitiveType::Triangles, _mesh.index_count(), 0));
+
+	Services::get().get_renderer()->submit_render_commands(_render_cmds);
+}
+
 unsigned int Terrain::_flatten_coord(unsigned int x, unsigned int y) const
 {
 	return ((_width + 1) * y) + x;
+}
+
+glm::vec2 Terrain::_unflatten_coord(unsigned int index) const
+{
+	glm::vec2 val;
+
+	val.x = (index % (_width + 1));
+	val.y = (index / (_width + 1));
+
+	return val;
+}
+
+glm::vec3 Terrain::_index_to_world(unsigned int x, unsigned int y) const
+{
+	float half_width = (float)_width * 0.5f;
+	float half_height = (float)_height * 0.5f;
+
+	glm::vec3 val = glm::vec3(0.0f);
+
+	val.x = x - half_width;
+	/*val.y = y - half_height;*/
+	val.z = y - half_height;
+	
+	return val;
 }
--- haksaengine/haksaengine/scene/terrain.h
+++ haksaengine/haksaengine/scene/terrain.h
@@ -3,6 +3,8 @@
 #include <glm/glm.hpp>
 
 #include "gfx/terrain_mesh.h"
+#include "gfx/shader.h"
+#include "gfx/render_command.h"
 
 class Terrain
 {
@@ -19,11 +21,20 @@ public:
 	// Get a terrain vertex by clamping a world position to closest vertex
 	const TerrainVertex& get_vertex(const glm::vec3 position) const;
 
+	// Generate draw commands for this piece of terrain
+	void draw(void);
+
 private:
 
 	unsigned int _flatten_coord(unsigned int x, unsigned int y) const;
+	glm::vec2    _unflatten_coord(unsigned int index) const;
+
+	glm::vec3 _index_to_world(unsigned int x, unsigned int y) const;
 
 	unsigned int _width;
 	unsigned int _height;
 	TerrainMesh _mesh;
+	const Shader* _shader;
+
+	std::vector<const RenderCommand*> _render_cmds;
 };
