9914097e72660aa247e028343569b969941b6279
Alastair Paterson <alas.paterson@gmail.com>
2018-08-17 04:37:15 +0100

    Updated game project main.cpp for testing

--- haksaengine/game/src/main.cpp
+++ haksaengine/game/src/main.cpp
@@ -27,19 +27,6 @@
 
 int main(int argc, char** argv)
 {
-	Triangle triangle(
-		glm::vec3(100.0f, 0.0f, 0.0f),
-		glm::vec3(200.0f, 0.0f, 0.0f),
-		glm::vec3(100.0f, 1.0f, 0.0f)
-	);
-
-	AABB aabb(
-		glm::vec3(-10.0f, -10.0f, -10.0f),
-		glm::vec3(10.0f, 10.0f, 10.0f)
-	);
-
-	bool xsect = aabb.intersect(triangle);
-
 	std::mt19937 generator(999);
 	std::uniform_real_distribution<float> dis(0.0f, 1.0f);
 
@@ -49,7 +36,7 @@ int main(int argc, char** argv)
 	Services::get().get_asset_manager()->set_asset_directory_path("../../../assets/");
 
 	unsigned int camera_blueprint_id = Services::get().get_asset_manager()->load_asset<Blueprint>("camera.bpr");
-	unsigned int test_blueprint_id = Services::get().get_asset_manager()->load_asset<Blueprint>("archer.bpr");
+	unsigned int test_blueprint_id = Services::get().get_asset_manager()->load_asset<Blueprint>("units/archer.bpr");
 	unsigned int crate_bpr_id = Services::get<AssetManager>()->load_asset<Blueprint>("crate.bpr");
 
 	const Blueprint& camera_blueprint = Services::get().get_asset_manager()->get_asset<Blueprint>(camera_blueprint_id);
@@ -89,42 +76,6 @@ int main(int argc, char** argv)
 
 	Services::get().get_system_manager()->create<CameraControllerScript>(SystemOrdering(0, UpdatePriority::POSTINPUT, 0));
 	Services::get().get_system_manager()->create<InputScript>(SystemOrdering(0, UpdatePriority::POSTINPUT, 1));
-
-	auto xsects = Services::get().get_scene_manager()->get_terrain()->intersect(entity);
-
-	glm::vec3 average_normal(0.0f, 0.0f, 0.0f);
-	glm::vec3 average_pos(0.0f, 0.0f, 0.0f);
-	for (auto tri : xsects)
-	{
-		average_normal += tri->normal;
-		average_pos += tri->v1;
-		average_pos += tri->v2;
-		average_pos += tri->v3;
-	}
-
-	average_normal = glm::normalize(average_normal / (float)xsects.size());
-	average_pos = average_pos / (float)xsects.size();
-
-	Collider* collider = entity->get_component<Collider>();
-	AABB tmp;
-
-	do 
-	{
-		glm::mat4 locscale = glm::mat4(1.0f);
-		locscale = glm::scale(locscale, transform->get_scale());
-		locscale = glm::translate(locscale, transform->get_position());
-
-		glm::vec3 min = locscale * glm::vec4(collider->aabb.min, 1.0f);
-		glm::vec3 max = locscale * glm::vec4(collider->aabb.max, 1.0f);
-
-		tmp = AABB(min, max);
-
-		if (tmp.intersect(average_pos))
-		{
-			transform->translate_by(average_normal);
-		}
-	} 
-	while (tmp.intersect(average_pos));
 
 	e.run();
 
